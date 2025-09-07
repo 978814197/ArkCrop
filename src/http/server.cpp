14263f3bf7a7df266850d4e3fdde1f001ae7b4c1
屿你有关 <ccsssng@gmail.com>
2025-09-07 15:10:21 +0800

    feat: 重构服务器代码以使用 Boost.Asio 命名空间，增强可读性和一致性

--- src/http/server.cpp
+++ src/http/server.cpp
@@ -1,22 +1,33 @@
 #include "server.h"
 
 #include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/detached.hpp>
 #include <boost/asio/signal_set.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/json.hpp>
 
 namespace beast = boost::beast;
 namespace http = beast::http;
-namespace json = boost::json;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
 
 
-// --------- 监听器：接受连接并 co_spawn 会话 ----------
-static awaitable<void> listener(const std::string host, unsigned short port)
+/**
+ * 监听器协程
+ * @param host 监听地址
+ * @param port 监听端口
+ * @return 协程句柄
+ */
+net::awaitable<void> listener(const std::string& host, std::uint16_t port)
 {
-    auto ex = co_await boost::asio::this_coro::executor;
+    // 获取当前协程的执行器（executor）
+    auto ex = co_await net::this_coro::executor;
 
-    tcp::endpoint ep{boost::asio::ip::make_address(host), port};
+    // 创建并配置 TCP 接受器
+    tcp::endpoint ep{net::ip::make_address(host), port};
     tcp::acceptor acc(ex);
 
     acc.open(ep.protocol());
@@ -27,24 +38,32 @@ static awaitable<void> listener(const std::string host, unsigned short port)
     for (;;)
     {
         tcp::socket sock(ex);
-        co_await acc.async_accept(sock, use_awaitable);
-        // boost::asio::co_spawn(ex, http_session(std::move(sock)), boost::asio::detached);
+        co_await acc.async_accept(sock, net::use_awaitable);
+
+        sock.set_option(net::ip::tcp::no_delay(true));
+
+        // TODO: 启动会话协程
+        // net::co_spawn(ex, http_session(std::move(sock)), net::detached);
     }
 }
 
+
 /**
- * 启动HTTP服务器
+ * 运行 HTTP 服务器
  * @param host 监听地址
  * @param port 监听端口
  */
 void run_server(const std::string& host, const int port)
 {
-    boost::asio::io_context io;
+    net::io_context io;
 
     // 支持 Ctrl+C / kill 优雅退出
-    boost::asio::signal_set signals(io, SIGINT, SIGTERM);
-    signals.async_wait([&](auto, auto) { io.stop(); });
+    net::signal_set signals(io, SIGINT, SIGTERM);
+    signals.async_wait([&](auto /*ec*/, auto /*signo*/) { io.stop(); });
 
-    boost::asio::co_spawn(io, listener(host, static_cast<unsigned short>(port)), boost::asio::detached);
+    // 启动监听器协程
+    net::co_spawn(io, listener(host, static_cast<std::uint16_t>(port)), net::detached);
+
+    // 运行 IO 上下文
     io.run();
 }
--- src/http/server.h
+++ src/http/server.h
@@ -1,17 +1,6 @@
 #pragma once
 
 #include <string>
-#include <boost/asio.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/json.hpp>
-
-using boost::asio::ip::tcp;
-using boost::asio::awaitable;
-using boost::asio::use_awaitable;
-
-namespace http = boost::beast::http;
-namespace json = boost::json;
-
 
 /**
  * 启动HTTP服务器
